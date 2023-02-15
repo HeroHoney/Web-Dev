aa97adfe9c4199aff018cae84cea86ed68580ce2
pp221B030782 <dmaratovich03@gmail.com>
2023-02-15 18:40:09 +0600

    now I have tasks added and removed

---
+++ Lab 3/ToDo app/1.js
@@ -0,0 +1,23 @@
+let input = document.querySelector('.inp');
+function push(){
+    document.getElementById('tasks').innerHTML += 
+    `
+        <div class="task">
+            <span id="taskname">
+                ${input.value}
+            </span>
+            <button class="delete">
+                <i class="far fa-trash-alt"></i>
+            </button>
+        </div>
+    `
+    ;
+
+    var current_tasks = document.querySelectorAll(".delete");
+    for(var i=0; i<current_tasks.length; i++){
+        current_tasks[i].onclick = function(){
+            this.parentNode.remove();
+        }
+    }
+    
+}
