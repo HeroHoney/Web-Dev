ef9f3f1f9b80bba52a442a87945a89c0904aca79
pp221B030782 <dmaratovich03@gmail.com>
2023-02-22 01:40:05 +0600

    added 10 products and ratings for task2

--- Lab 4/qddl9q--run (2)/src/app/products.ts
+++ Lab 4/qddl9q--run (2)/src/app/products.ts
@@ -3,26 +3,79 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  ratings: number
 }
 
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'Mont Blanc Montblanc Explorer',
+    price: 31734,
+    description: 'тип: парфюмерная вода',
+    ratings: 5
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Avon INCANDESSENCE',
+    price:4479,
+    description: 'объем: 50 мл,для женщин',
+    ratings: 5
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'LANVIN Eclat dArpege',
+    price: 21291,
+    description: 'тип: парфюмерная вода EDP',
+    ratings: 5
+  },
+  {
+    id: 4,
+    name: 'Avon Wild Country',
+    price: 3800,
+    description: 'Est.1967 туалетная вода EDT 75 мл',
+    ratings: 5
+  },
+  {
+    id: 5,
+    name: 'Versace Man Eau',
+    price: 2935,
+    description: 'туалетная вода EDT 5 мл',
+    ratings: 5
+  },
+  {
+    id: 6,
+    name: 'Tiziana Terenzi Kirke',
+    price: 59990,
+    description: 'парфюмерная вода EDP 100 мл',
+    ratings: 5
+  },
+  {
+    id: 7,
+    name: 'Antonio Banderas Blue Seduction ',
+    price: 10810,
+    description: ' туалетная вода EDT 100 мл',
+    ratings: 5
+  },
+  {
+    id: 8,
+    name: 'Avon PUR BLANCA',
+    price:3243,
+    description: 'для нее парфюмерная вода EDP 50 мл',
+    ratings: 5
+  },
+  {
+    id: 9,
+    name: 'Al-Rayan 212 Men',
+    price: 2000,
+    description: 'парфюмерное масло 10 мл',
+    ratings: 5
+  },
+  {
+    id: 10,
+    name: 'Avon Full Speed',
+    price: 3908,
+    description: 'парфюмерная вода EDP 75 мл',
+    ratings: 5
   }
 ];
 
