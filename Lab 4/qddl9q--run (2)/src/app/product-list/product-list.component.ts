2828fc917a7b573e81a297a65859e0d77601bcfa
pp221B030782 <dmaratovich03@gmail.com>
2023-02-22 02:31:14 +0600

    finished task2, added share Whatsapp

--- Lab 4/qddl9q--run (2)/src/app/product-list/product-list.component.ts
+++ Lab 4/qddl9q--run (2)/src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component} from '@angular/core';
 
-import { products } from '../products';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -10,8 +10,9 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(product: Product) {
+    const url=encodeURI(product.link);
+    window.open(`https://web.whatsapp.com://send?text=${url}`);
   }
   onNotify() {
     window.alert('You will be notified when the product goes on sale');
