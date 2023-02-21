ac59b0f57f077025a3ae55bcd7f8aab221742680
pp221B030782 <dmaratovich03@gmail.com>
2023-02-21 17:32:54 +0600

    Finished task1

--- Lab 4/qddl9q--run (2)/src/app/product-details/product-details.component.ts
+++ Lab 4/qddl9q--run (2)/src/app/product-details/product-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
-
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -18,7 +18,14 @@ export class ProductDetailsComponent implements OnInit{
     this.product = products.find(product => product.id === productIdFromRoute);
   }
   product: Product | undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
 
-  constructor(private route: ActivatedRoute) { }
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 }
 
