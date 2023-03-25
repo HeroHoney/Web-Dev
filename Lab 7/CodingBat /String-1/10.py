3dd79cb4466b6c62d9dc4036db90145ab231eca4
pp221B030782 <dmaratovich03@gmail.com>
2023-03-26 00:58:32 +0600

    finished string1

---
+++ Lab 7/CodingBat /String-1/10.py
@@ -0,0 +1,6 @@
+def non_start(a, b):
+  """
+  Given 2 strings, return their concatenation, except omit the first char 
+  of each. The strings will be at least length 1. 
+  """
+  return a[1:]+b[1:]
---
+++ Lab 7/CodingBat /String-1/7.py
@@ -0,0 +1,7 @@
+
+def first_half(str):
+  """
+  Given a string of even length, return the first half. So the string "WooHoo"
+  yields "Woo". 
+  """
+  return str[:len(str)/2]
