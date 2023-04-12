5339c0359dded14d93d581f443c69142c87f4461
pp221B030782 <dmaratovich03@gmail.com>
2023-04-12 11:27:11 +0600

    finish

--- Lab 9/hh_back/hh_back/urls.py
+++ Lab 9/hh_back/hh_back/urls.py
@@ -1,14 +1,7 @@
-from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from api import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('api/companies/', views.CompanyList.as_view()),
-    path('api/companies/<int:pk>/', views.CompanyDetail.as_view()),
-    path('api/companies/<int:id>/vacancies/', views.CompanyVacancies.as_view()),
-    path('api/vacancies/', views.VacancyList.as_view()),
-    path('api/vacancies/<int:pk>/', views.VacancyDetail.as_view()),
-    path('api/vacancies/top_ten/', views.TopTenVacancies.as_view()),
+    path('admin/', admin.site.urls),
+    path('api/',include('api.urls'))
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
