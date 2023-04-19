e988f5dec7917f1b623ed1d1e3e1b910f2acd100
pp221B030782 <dmaratovich03@gmail.com>
2023-04-19 17:05:05 +0600

    finished

--- Lab 10/hh_back/api/views.py
+++ Lab 10/hh_back/api/views.py
@@ -3,6 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from api.models import Company, Vacancy
 from api.serializers import CompanySerializer, VacancySerializer
+from django.shortcuts import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 def company_list(request):
@@ -80,3 +84,42 @@ def vacancies_of_company(request, company_id):
     vacancies = Vacancy.objects.filter(company=company_id)
     serializer = VacancySerializer(vacancies, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+class CompanyList(APIView):
+    def get(self, request):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CompanyDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Company.objects.get(pk=pk)
+        except Company.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        company = self.get_object(pk)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        company = self.get_object(pk)
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        company = self.get_object(pk)
+        company.delete()
+        return Response({'deleted': True})
